7774b4f5a9c3c7c8c9ebec2949155d427df5da53
rahulbuildsstuff <rahulnalla.tech@gmail.com>
2026-04-11 02:24:09 +0530

    initialised server.js

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const taskRoutes = require("./routes/tasks");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/tasks", taskRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
